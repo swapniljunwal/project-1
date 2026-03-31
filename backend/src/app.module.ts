dac4e836f18901665cc626bcc5fa41ca55d8da8c
swapniljunwal <swapniljunwal07@gmail.com>
2026-03-31 16:05:52 +0530

    Updated docker-compose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URL as string), // ✅ ENV USED
+    TasksModule,
+  ],
 })
 export class AppModule {}
